b364560b80c2d02ea598b4ac1648b40508ea1b06
adan.tarazona <adangabriel1@gmail.com>
2023-01-23 22:11:47 -0300

    cambios en css para modal

--- modal.js
+++ modal.js
@@ -1,24 +1,20 @@
-const contenedorModal = document.getElementsByClassName('modal-contenedor')[0];
-const botonAbrirModal = document.getElementById('boton-carrito');
-const botonCerrarModal = document.getElementById('carritoCerrar');
-const modalCarrito = document.getElementsByClassName('modal-carrito')[0];
+const contenedorModal = document.getElementsByClassName("modal-contenedor")[0];
+const botonAbrir = document.getElementById("boton-carrito");
+const botoCerrar = document.getElementById("carritoCerrar");
+const modalCarrito = document.getElementsByClassName("modal-carrito")[0];
 
-contenedorModal.addEventListener('click',()=>{
-    contenedorModal.classList.toggle('modal-active')
-
+contenedorModal.addEventListener("click", () => {
+  contenedorModal.classList.toggle("modal-active");
 });
 
-botonAbrirModal.addEventListener('click',()=>{
-    botonAbrirModal.classList.toggle('modal-active');
-    
-})
+botonAbrir.addEventListener("click", () => {
+  contenedorModal.classList.toggle("modal-active");
+});
 
-botonCerrarModal.addEventListener('click',()=>{
-    botonCerrarModal.classList.toggle('modal-active');
-    
-})
+botoCerrar.addEventListener("click", () => {
+  contenedorModal.classList.toggle("modal-active");
+});
 
-modalCarrito.addEventListener('click',(event)=>{
-    event.stopPropagation();
-    
-})
+modalCarrito.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
